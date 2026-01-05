cbc13d628f93ed1dab519578edbd5015d5063d21
RaphouCode <b.raphaelmail@gmail.com>
2026-01-05 22:55:08 +0100

    Ajout des images en changements esthetiques

--- src/app/shared/components/box-card/box-card.component.ts
+++ src/app/shared/components/box-card/box-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { Box } from '../../../core/models/box.model';
 
 @Component({
     selector: 'app-box-card',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     templateUrl: './box-card.component.html',
     styleUrl: './box-card.component.scss'
 })
